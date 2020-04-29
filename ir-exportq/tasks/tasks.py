2181fbc428966e5dd7bb76d59a95818853263e75
Daniel Duc Trinh <dutr5288@colorado.edu>
2020-04-29 15:13:29 -0600

    add: task to export ir data + upload to s3

--- ir-exportq/tasks/tasks.py
+++ ir-exportq/tasks/tasks.py
@@ -1,12 +1,92 @@
 from celery.task import task
+import json
+import requests
+import csv
+import time
+from datetime import datetime
+import boto3
+import os
+from botocore.exceptions import ClientError
+
+workTypeDict = {
+    'GraduateThesisOrDissertation': 'graduate_thesis_or_dissertations',
+    'UndergraduateHonorsThesis': 'undergraduate_honors_theses',
+    'Dataset': 'datasets',
+    'Article': 'articles',
+    'Presentation': 'presentations',
+    'ConferenceProceeding': 'conference_proceedings',
+    'Book': 'books',
+    'BookChapter': 'book_chapters',
+    'Report': 'reports',
+    'Default': 'defaults'
+}
 
 
-#Example task
+def uploadToS3():
+    s3_client = boto3.client('s3')
+    filePath = datetime.now().strftime('%Y-%m-%d') + '-ir-export.csv'
+    if os.path.isfile(filePath):
+        try:
+            response = s3_client.upload_file(
+                filePath, 'cubl-ir-bepress', "ir-exports/" + filePath)
+            os.remove(filePath)
+        except ClientError as e:
+            return {'message':'unable to upload to s3. Check log for more information.'}
+        return {'message': 'File: ' + filePath + ' has been uploaded to S3.'}
+    else:
+        return {'message': 'File is not exits'}
 @task()
-def add(x, y):
-    """ Example task that adds two numbers or strings
-        args: x and y
-        return addition or concatination of strings
-    """
-    result = x + y
-    return result
+def runExport():
+    url = 'https://scholar.colorado.edu/catalog.json?per_page=100&q=&search_field=all_fields'
+    initPage = url + '&page=1'
+    total_pages = requests.get(initPage).json()['response']['pages']['total_pages']
+    fields = ['Title', 'Academic Affilation', 'Resource Type', 'URL']
+    rows = []
+    links = []
+    fileName = datetime.now().strftime('%Y-%m-%d') + '-ir-export.csv'
+    countRecords = 0;
+    for pageNumber in range(1):
+        pageNumber = pageNumber + 1
+        pageURL = url + '&page=' + str(pageNumber)
+        time.sleep(1)
+        for doc in requests.get(pageURL).json()['response']['docs']:
+            links = []
+            try:
+                title = ', '.join(doc['title_tesim'])
+            except:
+                title = 'error'
+            try:
+                if 'Collection' in doc['has_model_ssim']:
+                    academic = ''
+                else:
+                    academic = ', '.join(doc['academic_affiliation_tesim'])
+            except:
+                academic = 'error'
+            try:
+                if 'Collection' in doc['has_model_ssim']:
+                    resource = ''
+                else:
+                    resource = ', '.join(doc['resource_type_tesim'])
+            except:
+                resource = 'error'
+
+            try:
+                if 'Collection' in doc['has_model_ssim']:
+                    link = 'https://scholar.colorado.edu/collections/' + doc['id']
+                else:
+                    works = doc['has_model_ssim']
+                    for work in works:
+                        links.append('https://scholar.colorado.edu/concern/' +
+                                    workTypeDict[work] + '/' + doc['id'])
+                    link = ', '.join(links)
+            except:
+                link = 'error'
+            rows.append([title, academic, resource, link])
+            countRecords = countRecords + 1
+
+    with open(fileName, 'w') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(fields)
+        csvwriter.writerows(rows)
+    print('Done. Total: ' + str(countRecords) + ' records has been exported.')
+    return uploadToS3()
--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='ir-exportq',
-      version='0.0',
+      version='1.0',
       packages=find_packages(),
       install_requires=[
           'celery',
           'pymongo',
+          'requests',
+          'boto3'
       ],
       )
