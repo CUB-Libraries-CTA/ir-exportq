ba3b16fcbcbc93614275aec0ba1fdb5861adc5e9
Mark Stacy <mbstacy@gmail.com>
2020-04-29 14:13:45 -0600

    removed version for celery and pymongo

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(name='ir-exportq',
       version='0.0',
-      packages= find_packages(),
+      packages=find_packages(),
       install_requires=[
-          'celery==3.1.22',
-          'pymongo==3.2.1',
+          'celery',
+          'pymongo',
       ],
-)
+      )
